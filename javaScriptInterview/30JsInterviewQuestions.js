f973b65cf7854d558230498f76f0764eb73581cf
shreekanthlakka <shreekanth.lakka@hotmail.com>
2024-01-07 21:54:59 +0530

    added the interview Questions

---
+++ javaScriptInterview/30JsInterviewQuestions.js
@@ -0,0 +1,153 @@
+/**
+ *
+ * 1 What is Js and its key features
+ */
+
+/**
+ *
+ * 2 What are the difference between null and Undefined
+ */
+
+/**
+ *
+ * 3 what is closures in javascript and why is it important
+ */
+
+/**
+ *
+ * 4 Explain the event delegation concept
+ */
+
+/**
+ *
+ * 5 Difference between let , const and var
+ */
+
+/**
+ *
+ * 6 what is promise and how it works
+ */
+
+/**
+ *
+ * 7 what is the purpose of async and await in javascript
+ */
+
+/**
+ *
+ * 8 Describe the concept of hoisting in javaScript
+ */
+
+/**
+ *
+ * 9 what is the event loop in JS and how does it work
+ */
+
+/**
+ *
+ * 10 Explain the concept of prototype inheritance
+ */
+
+/**
+ *
+ * 11 Explain the difference between == and ===
+ */
+
+/**
+ *
+ * 12 Explain This Keyword
+ */
+
+/**
+ *
+ * 13 how does JS handle async Code in execution
+ *
+ *         -----callbacks
+ *         -----promises
+ *         -----async/await
+ */
+
+/**
+ *
+ * 14 what is the purpose of bind method in JavaScript
+ */
+
+/**
+ *
+ * 15 Explain the concept of callback Hell and how to avoid it
+ */
+
+/**
+ *
+ * 16 Explain the difference between map() , forEach() and filter()
+ */
+
+/**
+ *
+ * 17 what is Event bubbling in javaScript
+ */
+
+/**
+ *
+ * 18 what is the purpose of localStorage and sessionStorage
+ */
+
+/**
+ *
+ * 19 Explain same origin policy and its importance
+ */
+
+/**
+ *
+ * 20 How does JavaScript handle async code execution
+ */
+
+/**
+ *
+ * 21 JS event loop and how does it work
+ */
+
+/**
+ *
+ * 22  Explain Reduce()
+ */
+
+/**
+ *
+ * 23 JavaScript promises and its states
+ */
+
+/**
+ *
+ * 24 Explain JS fetch() API
+ */
+
+/**
+ *
+ * 25 how JS handle variable scope
+ */
+
+/**
+ *
+ * 26 concepts of callback , callback function and higher-order functions
+ */
+
+/**
+ *
+ * 27 What is event delegation and why is it useful
+ */
+
+/**
+ *
+ * 28 What are arrow functions in JS and how do they differ from regular regular functions
+ */
+
+/**
+ *
+ * 29 What is the diff b/w .call() and .apply()
+ */
+
+/**
+ *
+ * 30 Explain the concept of HOISTING in JS
+ */
